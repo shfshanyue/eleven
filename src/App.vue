4017a4aa12f55bb88a0827c962e99c603dee56b1
shfshanyue <xianger94@gmail.com>
2018-09-15 12:20:46 +0800

    Layout

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,18 @@
 export default {
 }
 </script>
+
+<style>
+view {
+  box-sizing: border-box;
+}
+
+.container {
+  padding: 0 35rpx; 
+}
+
+.title {
+  font-size: 1.6em;
+  margin: 5rpx 0;
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    hello 
+  <div class="container">
+    <div class="title">任选</div>
+    <div class="list">
+      <div v-for="way in ways" v-text="way" class="item"></div>
+    </div>
+    <div class="title">注数</div>
+    <div class="list">
+      <div v-for="count in counts" v-text="count" class="item"></div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +15,17 @@
 export default {
   data () {
     return {
+      ways: [
+        "一",
+        "二",
+        "三",
+        "四",
+        "五",
+        "六",
+        "七",
+        "八"
+      ],
+      counts: [1, 5, 10, 20]
     }
   },
 
@@ -19,3 +37,22 @@ export default {
 }
 </script>
 
+<style scoped>
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+}
+
+.item {
+  width: 150rpx;
+  height: 150rpx;
+  margin: 10rpx;
+
+  text-align: center;
+  line-height: 150rpx;
+
+  border: 4px solid red;
+  border-radius: 50%;
+}
+</style>

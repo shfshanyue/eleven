a7ee745de4f56dfe07e7c70d4ccacbf46de980ae
shfshanyue <xianger94@gmail.com>
2018-09-15 10:29:05 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    hello 
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
+
